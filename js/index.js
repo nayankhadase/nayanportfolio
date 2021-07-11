ba70a9e3d68d5dc78c1a56718da3811e4a40f92e
Nayan Khadase <72203524+nayankhadase@users.noreply.github.com>
2021-07-11 20:53:24 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// for profile button
+$(document).ready(
+            function() {
+                $("#btnnav").click(
+                    function() {
+                        $("#prop").toggle("slow");
+                        $("#iconbtn").toggleClass("far fa-window-close");
+                        $("#iconbtn").toggleClass("fas fa-bars ");
+                        
+                    }
+                )
+                $("#prop").click(
+                    function() {
+                        $("#prop").toggle("slow");
+                        $("#iconbtn").toggleClass("far fa-window-close");
+                        $("#iconbtn").toggleClass("fas fa-bars ");
+                    }
+                )
+
+            
+    
+                $("#about").hover(
+                function()
+                    {
+                        $("#getimg").animate({'width':"0",'width':"335px"},1000);
+                        $("#getimg").addClass("img-fluid");
+                        
+                    }
+                )
+    
+            }
+        )
